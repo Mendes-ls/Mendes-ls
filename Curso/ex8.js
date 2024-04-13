57d637a150b77ba5e43d2cb82e9b6612f02b84b9
Mendes-ls <vnmendes264@gmail.com>
2024-04-13 09:47:11 -0300

    novos projetos

--- Curso/ex8.js
+++ Curso/ex8.js
@@ -1,4 +1,4 @@
-var vel= 50.0
+var vel=78.2
 console.log(`A velocidade de seu carro é ${vel} km/h`)
 if(vel>60){
     console.log(`Vai mais devagar poha!`)
---
+++ Curso/ex9.js
@@ -0,0 +1,8 @@
+var pais = 'USA'
+console.log( `Seu pais é ${pais}`)
+if(pais != 'Brasil'){
+    console.log('Tu es um gringo')
+}
+else{
+    console.log('Tu és Brazuca')
+}
